7812eea4357ef4ea4c49ae71ab21ebfdd59673b4
debbysa <deby.silvia23@gmail.com>
2021-11-16 21:13:11 +0700

    [client-app] config app and postCreated

--- blog-client/src/App.js
+++ blog-client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PostCreate from './PostCreate'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+	return (
+		<div className="container">
+			<h1>Create Post</h1>
+			<PostCreate />
+		</div>
+	)
 }
-
-export default App;
---
+++ blog-client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+	const [title, setTitle] = useState('')
+
+	const onSubmit = async (event) => {
+		event.preventDefault()
+
+		await axios.post('http://localhost:4000', {
+			title,
+		})
+
+		setTitle('')
+	}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="form-control"
+					/>
+				</div>
+				<br />
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	)
+}
